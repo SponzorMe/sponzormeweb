cecf964979acee41e0f0032b9481771fd26f8544
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-10-31 19:39:44 -0300

    wp w/ environments

---
+++ wp-config.env.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Setup environments
+ *
+ * Set environment based on the current server hostname, this is stored
+ * in the $hostname variable
+ *
+ * You can define the current environment via:
+ *     define('WP_ENV', 'production');
+ *
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    1.0
+ * @author     Studio 24 Ltd  <info@studio24.net>
+ */
+/*
+ * Set environment based on hostname
+ *
+ * If you just use localhost for your local test environment then in place of:
+ *   case 'domain.dev':
+ *
+ * Just use:
+ *   case 'localhost':
+ *
+ */
+switch ($hostname) {
+    case 'local.sponzor.me':
+        define('WP_ENV', 'development');
+        break;
+
+    case 'web.sponzor.me':
+        define('WP_ENV', 'staging');
+        break;
+    case 'www.sponzor.me':
+    default:
+        define('WP_ENV', 'production');
+}
