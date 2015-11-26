f7a5b70cb1560a959444573476ffb4d25c918cd3
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-11-25 21:53:06 -0300

    add new videos. fixing in safari

--- wp-content/themes/sponzorme2015/footer.php
+++ wp-content/themes/sponzorme2015/footer.php
@@ -3,18 +3,50 @@
 
 		<footer class="mdl-mini-footer">
 			<div class="mdl-mini-footer__left-section"><a href="<?php site_url(); ?>?lang=es"><img height="32px" src="<?php echo get_template_directory_uri(); ?>/img/spanish.png"></a>&nbsp; <a href="<?php site_url(); ?>?lang=en"><img height="32px" src="<?php echo get_template_directory_uri(); ?>/img/english.png"></a>&nbsp; <a href="<?php site_url(); ?>?lang=pt-br"><img height="32px" src="<?php echo get_template_directory_uri(); ?>/img/pt.png"></a></div>
-        <div class="mdl-mini-footer__right-section">
-					Link1 - Link2
+        <div class="mdl-mini-footer__right-section footermini">
+					<?php
+					if(!isset($_GET['lang']))
+					{
+						$_GET['lang'] == "en";
+					}
+					else{
+					if($_GET['lang']=="es"){
+					?>
+					<a href="#">Testimonios</a> <a href="#">Soporte</a> <a href="#">Blog</a> <a href="#">Privacidad</a>
+					<?php
+					}
+					elseif($_GET['lang']=="en"){
+					?>
+					<a href="#">Testimonials</a> <a href="#">Support</a> <a href="#">Blog</a> <a href="#">Privacy</a>
+					<?php
+					}
+					elseif($_GET['lang']=="pt-br"){
+					?>
+					<a href="#">Depoimentos</a> <a href="#">Apoio</a> <a href="#">Blog</a> <a href="#">Privacidade</a>
+					<?php
+					}}
+					?>
+
+					<?php wp_nav_menu(array(
+						'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+						'container_class' => 'col-md-4 padding-top-10',         // class of container (should you choose to use it)
+						'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+						'menu_class' => 'centeredmenu',            // adding custom nav class
+						'theme_location' => 'footer-links',             // where it's located in the theme
+						'before' => '',                                 // before the menu
+						'after' => '',                                  // after the menu
+						'link_before' => '',                            // before each link
+						'link_after' => '',                             // after each link
+						'depth' => 0,                                   // limit the depth of the nav
+						'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+					)); ?>
         </div>
 
     </footer>
 		<figure id="video_player">
       <video autoplay poster="<?php echo get_template_directory_uri(); ?>/video/fondo.jpg" id="fondo">
-        <source src="<?php echo get_template_directory_uri(); ?>/video/video1_1.webm" type='video/webm; codecs="vp8, vorbis"'>
-        <source src="<?php echo get_template_directory_uri(); ?>/video/video2_1.webm" type='video/webm; codecs="vp8, vorbis"'>
-        <source src="<?php echo get_template_directory_uri(); ?>/video/video3_1.webm" type='video/webm; codecs="vp8, vorbis"'>
-        <source src="<?php echo get_template_directory_uri(); ?>/video/video4_1.webm" type='video/webm; codecs="vp8, vorbis"'>
-        <source src="<?php echo get_template_directory_uri(); ?>/video/video5_1.webm" type='video/webm; codecs="vp8, vorbis"'>
+        <source src="<?php echo get_template_directory_uri(); ?>/video/video1.mp4" type='video/mp4'>
+				<source src="<?php echo get_template_directory_uri(); ?>/video/video2.mp4" type='video/mp4'>
       </video>
     </figure>
 		<!-- Placed at the end of the document so the pages load faster -->
@@ -40,7 +72,7 @@
   (function() {
     function playVid(index) {
         source[1].src = vidDir + link_list[index] + ".webm";
-      //source[0].src = vidDir + link_list[index] + ".mp4";
+        source[0].src = vidDir + link_list[index] + ".mp4";
         currentVid = index;
         video.load();
         video.muted = true;
